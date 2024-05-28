38e1f82a61f6a360d16a72f64edca18a4761ed58
Tom177 <tatumac@gmail.com>
2024-05-28 14:48:12 +0900

    ,to\n

--- main2.py
+++ main2.py
@@ -42,14 +42,14 @@ for i, prefecture in enumerate(PREFECTURES, 1):
 #        if df.columns.get_loc(col) != 6:  # 7列目のインデックスは6
     #        df[col] = df[col].str.replace('\n', ' ', regex=True)
 #            df[col] = df[col].replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
-    # 改行コードを削除
-    #df = df.replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
+    # カンマを改行コードに変換
+    df = df.replace(',', '\n', regex=True)
     #時間表記の「~」を「-」に変換
     df = df.replace("~", "-", regex=True).replace("～", "-", regex=True)
     #時間表記の「~」を「-」に変換
     df = df.replace("~", "-", regex=True)
     # データが2つ未満の行は不要な可能性が高いので行を削除 & 列名に欠損値がある場合も列ごと削除
-#    result_df = df.dropna(thresh=2).dropna(subset=[df.index[0]], axis=1)
+    result_df = df.dropna(thresh=2).dropna(subset=[df.index[0]], axis=1)
 
     prefecture_number = str(i).zfill(2)
     result_df.to_csv(f"./output_files/{prefecture_number}_{prefecture}.csv", header=False, index=False)
