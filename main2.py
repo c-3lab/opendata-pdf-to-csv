ed90ad7cb78c9fdac3c153eb8c96e4c10b7ff4e6
Tom177 <tatumac@gmail.com>
2024-06-04 09:52:06 +0900

    test

--- main2.py
+++ main2.py
@@ -39,13 +39,15 @@ for i, prefecture in enumerate(PREFECTURES, 1):
     #時間表記の「~」を「-」に変換
     df = df.replace("~", "-", regex=True).replace("～", "-", regex=True)
     #「"」を削除
-    df = df.replace('"', '', regex=True)
+    
+    #df = df.replace('"', '', regex=True)
 
     # 7列目のみ改行コードを残しそれ以外は改行コードを削除
     for col in df.columns:
         if df.columns.get_loc(col) != 6:  # 7列目のインデックスは6
             df[col] = df[col].replace('\n', '', regex=True).replace('\r', '', regex=True).replace('\r\n', '', regex=True).replace('\n\r', '', regex=True)
             df[col] = df[col].replace('、', '\n', regex=True).replace(':', '\n', regex=True)
+    
     # データが2つ未満の行は不要な可能性が高いので行を削除 & 列名に欠損値がある場合も列ごと削除
     result_df = df.dropna(thresh=2).dropna(subset=[df.index[0]], axis=1)
 
